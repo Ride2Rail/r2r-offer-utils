f7d9bbcf25442030b2fce5258165d3861957f634
Cristian Consonni <cristian@balist.es>
2021-03-18 13:45:12 +0100

    Minor fixes

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-"""compressed_stream
-   -----------------
-   A simple python module to handle streams of compressed files.
-
+"""# Ride2Rail offer categorizer utils
 """
 
 from setuptools import setup, find_packages
 
 setup(
     name='r2r_offer_utils',
-    version='0.0.2',
+    version='0.1',
     author='',
     author_email='',
     license='MIT',
     description='',
     long_description=__doc__,
-    url='https://github.com/CristianCantoro/compressed_stream',
+    url='https://github.com/Ride2Rail/r2r-offer-utils',
     packages=find_packages(),
     entry_points={},
     options={
