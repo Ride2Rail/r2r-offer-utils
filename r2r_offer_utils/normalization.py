bb97e723767cd722de9d949dc2fd3c8b042f4956
lubos <lubos31262@gmail.com>
2021-03-23 08:56:57 +0100

    zscore calculation

--- r2r_offer_utils/normalization.py
+++ r2r_offer_utils/normalization.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
 from typing import Mapping
-
+import math
 
 def zscore(offers: Mapping) -> Mapping:
-    # do stuff
-    return {o: 0 for o in offers}
+
+    n          = 0
+    sum        = 0.0
+    sum_square = 0.0
+
+    for o in offers:
+        value = offers[o]
+        if not(value is None):
+            n = n + 1
+            sum = sum + value
+            sum_square = sum_square + value*value
+    average = sum/n
+    std     = math.sqrt(sum_square/n - (sum/n)*(sum/n))
+
+    z_scores = {}
+    for o in offers:
+        value = offers[o]
+        if not (value is None):
+            z_scores[o] = (value - average)/std
+    return z_scores
 
 
 if __name__ == '__main__':
