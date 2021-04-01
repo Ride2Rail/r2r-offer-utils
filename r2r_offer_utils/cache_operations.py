a3cb324e3fe7e52f95a0f243c7ea9f6de56a184c
lubos <lubos31262@gmail.com>
2021-04-01 17:18:21 +0200

    minor updates (corrections)

--- r2r_offer_utils/cache_operations.py
+++ r2r_offer_utils/cache_operations.py
@@ -14,38 +14,32 @@ def extract_data_from_cache(
     output_offer_level_items   = {}
     output_tripleg_level_items = {}
 
-    #offer_ids  = pa_cache.jsonget('{}:offers'.format(pa_request_id))
     offer_ids = pa_cache.lrange('{}:offers'.format(pa_request_id), 0, -1)
-
     output_offer_level_items["offer_ids"] = offer_ids
     if offer_ids  is not None:
         for offer in offer_ids:
             output_offer_level_items[offer] = {}
-            if len(pa_tripleg_level_items) > 0:
-                output_tripleg_level_items[offer] = {}
             for offer_level_item in pa_offer_level_items:
                 # assembly key for offer level
-                temp_key  = pa_request_id + ":" + offer + ":" + offer_level_item
+                temp_key  = "{}:{}:{}".format(pa_request_id,offer,offer_level_item)
                 # extract offer level data from cache
-                #temp_data = pa_cache.jsonget(temp_key)
                 if (offer_level_item == "bookable_total") or (offer_level_item == "complete_total"):
                     temp_data = pa_cache.hgetall(temp_key)
                 else:
                     temp_data = pa_cache.get(temp_key)
                 output_offer_level_items[offer][offer_level_item] = temp_data
-                # extract information at the tripleg level
-                if len(pa_tripleg_level_items) > 0:
-                    temp_key      = pa_request_id + ":" + offer + ":" + "legs"
-                    #tripleg_ids   = pa_cache.jsonget(temp_key)
-                    tripleg_ids  = pa_cache.lrange(temp_key, 0, -1)
-                    output_tripleg_level_items[offer]["triplegs"] = tripleg_ids
-                    for tripleg_id in tripleg_ids:
-                        output_tripleg_level_items[offer][tripleg_id] = {}
-                        for tripleg_level_item in pa_tripleg_level_items:
-                            temp_key = pa_request_id + ":" + offer + ":" + tripleg_id + ":" + tripleg_level_item
-                            #tripleg_data   = pa_cache.jsonget(temp_key)
-                            tripleg_data  = pa_cache.get(temp_key)
-                            output_tripleg_level_items[offer][tripleg_id][tripleg_level_item] = tripleg_data
+            # extract information at the tripleg level
+            output_tripleg_level_items[offer] = {}
+            if len(pa_tripleg_level_items) > 0:
+                temp_key      = "{}:{}:legs".format(pa_request_id,offer)
+                tripleg_ids  = pa_cache.lrange(temp_key, 0, -1)
+                output_tripleg_level_items[offer]["triplegs"] = tripleg_ids
+                for tripleg_id in tripleg_ids:
+                    output_tripleg_level_items[offer][tripleg_id] = {}
+                    for tripleg_level_item in pa_tripleg_level_items:
+                        temp_key = "{}:{}:{}:{}".format(pa_request_id, offer, tripleg_id,tripleg_level_item)
+                        tripleg_data  = pa_cache.get(temp_key)
+                        output_tripleg_level_items[offer][tripleg_id][tripleg_level_item] = tripleg_data
     return output_offer_level_items, output_tripleg_level_items
 #############################################################################
 #############################################################################
